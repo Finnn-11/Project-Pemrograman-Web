0273b82d26267c193e43499a67f0d9705e04997c
Finnn-11 <findixie01@gmail.com>
2025-07-02 18:45:23 +0700

    Delete includes directory

--- includes/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $db = new PDO('sqlite:' . __DIR__ . '/../database.sqlite');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Koneksi database gagal: " . $e->getMessage());
-}
-?>
